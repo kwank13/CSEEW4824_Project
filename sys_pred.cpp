8b69984736cd02097f66712e8909f768ad7a1e92
Kevin Kwan <kjk2150@columbia.edu>
2017-05-03 13:10:30 -0400

    Changed code to remove buffer size - now code keeps all instrs until sys call

--- sys_pred.cpp
+++ sys_pred.cpp
@@ -17,22 +17,6 @@ vector< vector<string> > calls; // Vector of sys calls and associated instrs
 uint buf_size; // Queue size
 
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "trace.out", "specify file name for branch predictor output");
-// TODO: Test for all values of b
-KNOB<UINT32> KnobB(KNOB_MODE_WRITEONCE, "pintool", "b", "5", "Buffer size");
-
-// Prints system call # + arguments
-VOID PrintSys(ADDRINT ip, ADDRINT num, ADDRINT arg0, ADDRINT arg1, ADDRINT arg2, ADDRINT arg3, ADDRINT arg4, ADDRINT arg5)
-{
-	printf("0x%lx: %ld (0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)\n",
-			(unsigned long) ip,
-			(long) num,
-			(unsigned long) arg0,
-			(unsigned long) arg1,
-			(unsigned long) arg2,
-			(unsigned long) arg3,
-			(unsigned long) arg4,
-			(unsigned long) arg5);
-}
 
 // Adds system call # + args and current instructions in queue to a vector
 VOID GetSys(ADDRINT ip, ADDRINT num, ADDRINT arg0, ADDRINT arg1, ADDRINT arg2, ADDRINT arg3, ADDRINT arg4, ADDRINT arg5)
@@ -54,6 +38,7 @@ VOID GetSys(ADDRINT ip, ADDRINT num, ADDRINT arg0, ADDRINT arg1, ADDRINT arg2, A
 	for (deque<string>::iterator it = instr_queue.begin(); it != instr_queue.end(); it++){
 		temp.push_back(*it);
 	}
+	instr_queue.clear(); // Clear queue
 	temp.push_back(buf);
 	calls.push_back(temp); // Adds vector to vector of all sys calls
 }
@@ -62,28 +47,13 @@ VOID Instruction(INS ins, VOID *v)
 {
 	// If not a system call, put instruction into queue
 	// If queue is filled, pop first element and insert new instruction
-	if (!INS_IsSyscall(ins)){
-		if (instr_queue.size() == buf_size) {
-			instr_queue.pop_front();
-			instr_queue.push_back(INS_Disassemble(ins).c_str());
-		} else
 
+	if (!INS_IsSyscall(ins))
 			instr_queue.push_back(INS_Disassemble(ins).c_str());
-	}
 }
 
 // Functions called on system call
 VOID SyscallEntry(THREADID tid, CONTEXT *ctxt, SYSCALL_STANDARD std, void *v){
-/*
-	PrintSys(PIN_GetContextReg(ctxt, REG_INST_PTR),
-			PIN_GetSyscallNumber(ctxt, std),
-			PIN_GetSyscallArgument(ctxt, std, 0),
-			PIN_GetSyscallArgument(ctxt, std, 1),
-			PIN_GetSyscallArgument(ctxt, std, 2),
-			PIN_GetSyscallArgument(ctxt, std, 3),
-			PIN_GetSyscallArgument(ctxt, std, 4),
-			PIN_GetSyscallArgument(ctxt, std, 5));
-*/
 		GetSys(PIN_GetContextReg(ctxt, REG_INST_PTR),
 			PIN_GetSyscallNumber(ctxt, std),
 			PIN_GetSyscallArgument(ctxt, std, 0),
@@ -107,12 +77,7 @@ VOID Fini(int code, VOID * v) {
 	ofstream out;
 	filename = KnobOutputFile.Value();
 	out.open(filename.c_str());
-/*
-	// Prints queue
-	for (deque<string>::iterator it = instr_queue.begin(); it != instr_queue.end(); it++){
-		cout << *it << endl;
-	}
-*/
+
 	// Prints calls vector
 	for (vector< vector<string> >::iterator it = calls.begin(); it != calls.end(); it++){
 		for (vector<string>::iterator it2 = it->begin(); it2 != it->end(); it2++){
@@ -121,7 +86,6 @@ VOID Fini(int code, VOID * v) {
 		out << endl;
 	}
 	out.close();
-
 }
 
 // Called once prior to program execution
@@ -132,7 +96,6 @@ int main(int argc, CHAR *argv[]) {
         return Usage();
     }
 
-	buf_size = (KnobB.Value()); // Gets buffer size from user flag
 
     INS_AddInstrumentFunction(Instruction, 0);
 	PIN_AddSyscallEntryFunction(SyscallEntry, 0);
